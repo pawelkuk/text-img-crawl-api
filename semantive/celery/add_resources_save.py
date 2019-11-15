2e8289f5041ce54ff87fb7ba1a1e79542b092537
Paweł Kukliński <paffcio314@gmail.com>
2019-11-15 15:18:29 +0100

    Add tests to extract data, refactor logic to enable testing.

--- semantive/celery/add_resources_save.py
+++ semantive/celery/add_resources_save.py
@@ -8,20 +8,10 @@ def add_resource_save(name, api, celery, db):
 
     @celery.task(name='celery.save')
     def save(args):
-        if args['content'] == 'images':
-            img_dict = db['images']
-            data = []
-            for site in img_dict:
-                for image in site['data']:
-                    data.append([site['url'], image])
-
-        elif args['content'] == 'text':
-            text_dict = db['text']
-            data = []
-            for line in text_dict:
-                data.append([line['url'], line['data']])
-        else:
+        if args['content'] not in ['text', 'images']:
             return {'STATUS': 'FAILURE', }
+
+        data = extract_data(args['content'], db)
 
         with open('data.csv', "w") as csv_file:
             writer = csv.writer(csv_file, delimiter=',', quotechar='`')
@@ -49,3 +39,20 @@ def add_resource_save(name, api, celery, db):
                 'requested-content': args['content']
                 }
     api.add_resource(SaveContent, name)
+
+
+def extract_data(content_type, db):
+    data = []
+    if content_type not in db.keys():
+        return data
+    if content_type == 'images':
+        img_dict = db['images']
+        for site in img_dict:
+            for image in site['data']:
+                data.append([site['url'], image])
+
+    elif content_type == 'text':
+        text_dict = db['text']
+        for line in text_dict:
+            data.append([line['url'], line['data']])
+    return data
---
+++ tests/test_extract_data.py
@@ -0,0 +1,32 @@
+import pytest
+from semantive.celery.add_resources_save import extract_data
+import shelve
+
+
+def test_extract_data_empty_db(tmp_path):
+    file_ = tmp_path / "shelve"
+    db = shelve.open(str(file_))
+    assert extract_data('text', db) == []
+    assert extract_data('images', db) == []
+
+
+def test_extract_data_populated_db(tmp_path):
+    file_ = tmp_path / "shelve"
+    db = shelve.open(str(file_))
+    url = 'https://www.example.com'
+    db['text'] = [{
+        'url': url,
+        'data': 'some data',
+    }, ]
+    db['images'] = [{
+        'url': url,
+        'data': [url+'/1', url+'/2', ],
+    }, ]
+    extracted_text_data = [[url, 'some data', ], ]
+    extracted_image_data = [
+        [url, url+'/1', ],
+        [url, url+'/2', ]
+        ]
+    assert extract_data('text', db)[0] == extracted_text_data[0]
+    assert extract_data('images', db)[0] == extracted_image_data[0]
+    assert extract_data('images', db)[1] == extracted_image_data[1]
